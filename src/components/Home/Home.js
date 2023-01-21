34f4353e8361a40a3a4a1dbb92da7277e1107859
razikhaNawaz <razikhafaheem02@gmail.com>
2023-01-21 10:48:40 +0530

    sixth git

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
+import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import AuthContext from '../../Store/auth-context';
 
-const Home = (props) => {
+const Home = () => {
+  const authCtx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
+    
   );
 };
 
